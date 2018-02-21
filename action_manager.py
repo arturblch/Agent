9805c64f3db8ec41d7f3d0047595b0b44163febd
Artur <ar-2@tut.by>
2018-02-21 16:15:00 +0300

    update action_manager and add tests

--- action_manager.py
+++ action_manager.py
@@ -1,27 +1,30 @@
-# This file about actiion managment for agent
+# TODO(arturblch): add priority queue
+
+
 class ActionManager:
+    """ActionManager provide access to action queue
+    """
+
     def __init__(self):
         self.tick = 0
         self.cur_action = None
-        self.last_action = None
-        self.action_tick = 0
-
+        self.last_actions = dict()
         self.action_queue = []
 
+    # TODO(arturblch): check if action was in past
     def add_action(self, action):
-        for delay_actions in self.action_queue:
-            if delay_actions.equal(action):
-                self.action_queue.remove(delay_actions)
-        self.action_queue.add(action)
+        if action in self.action_queue:
+            self.action_queue.remove(action)
+        self.action_queue.append(action)
         return True
 
+    def renewActions(self):
+        pass
 
-    def execute_delay_move(self, move, world):
-        move = self.cur_action.execute() if self.cur_action else None
-        if move:
-            return move
-        elif (len(self.action_queue) > 0):
+    def next_command(self):
+        func = self.cur_action.next() if self.cur_action else None
+        if func:
+            return func
+        elif len(self.action_queue) > 0:
             self.cur_action = self.action_queue.pop(0)
-            move = self.cur_action.execute()
-        else:
-            return None
+            return self.cur_action.next()
--- test_actionManager.py
+++ test_actionManager.py
@@ -1,10 +1,37 @@
 import pytest
 from action_manager import ActionManager
+from action import Action
+from action import PRIORITY
+
+from pysc2.lib.actions import FUNCTIONS
+
 
 class TestActionManager:
     def test_add_action(self):
         action_manager = ActionManager()
+        act1 = Action.buildSCV(30, 30, 34, 34)
+        act2 = Action.buildSCV(30, 30, 34, 34)
+        act3 = Action.buildSCV(30, 3, 34, 34)
+
+        action_manager.add_action(act1)
+        action_manager.add_action(act2)
+        action_manager.add_action(act3)
+
+        assert len(action_manager.action_queue) == 2
 
 
-    def test_execute_delay_move(self):
+    def test_next_command(self):
         action_manager = ActionManager()
+        act1 = Action(None, FUNCTIONS.move_camera.function_type, PRIORITY.NORMAL)
+        act2 = Action(None, FUNCTIONS.move_camera.function_type, PRIORITY.NORMAL)
+        act1.commands.append(FUNCTIONS.move_camera([[1, 2]]))
+        act2.commands.append(FUNCTIONS.move_camera([[3, 4]]))
+
+        action_manager.add_action(act1)
+        action_manager.add_action(act2)
+
+        assert action_manager.next_command == FUNCTIONS.move_camera([[1, 2]])
+        assert action_manager.next_command == FUNCTIONS.move_camera([[3, 4]])
+        assert action_manager.next_command is None
+
+
